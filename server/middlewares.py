d2020b076cded0b32a80f607d5070d521f663482
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2020-08-03 17:10:31 +0300

    SWSS-8: Create view for budget

--- server/middlewares.py
+++ server/middlewares.py
@@ -1,7 +1,8 @@
 """This module provides middlewares for server application."""
 
-import jwt
+import json
 
+import jwt
 from aiohttp import web
 
 
@@ -14,7 +15,7 @@ async def auth_middleware(request, handler):
 
     if not token:
         return web.json_response(
-            data={"message": "You aren't authorized. Please provide authorization token."},
+            data={"success": False, "message": "You aren't authorized. Please provide authorization token."},
             status=401
         )
 
@@ -22,14 +23,30 @@ async def auth_middleware(request, handler):
         payload = jwt.decode(token, secret, algorithm=jwt_algorithm)
     except jwt.DecodeError:
         return web.json_response(
-            data={"message": "Wrong credentials. Your token is invalid."},
+            data={"success": False, "message": "Wrong credentials. Your token is invalid."},
             status=401
         )
     except jwt.ExpiredSignatureError:
         return web.json_response(
-            data={"message": "Wrong credentials. Your token is expired."},
+            data={"success": False, "message": "Wrong credentials. Your token is expired."},
             status=401
         )
 
     request.user_id = payload["user_id"]
+    return await handler(request)
+
+
+@web.middleware
+async def body_validator_middleware(request, handler):
+    """Check if provided body data for mutation methods is correct."""
+    if request.body_exists:
+        content = await request.read()
+        try:
+            request.body = json.loads(content)
+        except json.decoder.JSONDecodeError:
+            return web.json_response(
+                data={"success": False, "message": "Wrong input. Can't deserialize body input."},
+                status=400
+            )
+
     return await handler(request)
