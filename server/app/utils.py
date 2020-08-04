60819ade87d0161f29854371db79127ae202ee7a
Oleksii Petrushynskyi <petrushynskyi.oleksii@gmail.com>
2020-08-04 14:24:29 +0300

    SWSS-14: Create view for daily budget reports

---
+++ server/app/utils.py
@@ -0,0 +1,11 @@
+"""This module provides helper functionality."""
+
+from datetime import timedelta
+
+
+def generate_days_period(start_date, end_date):
+    """Yield days in provided date period."""
+    start_date = start_date.replace(hour=0, minute=0, second=0, microsecond=0)
+    days_count = (end_date - start_date).days + 1
+    for n_days in range(days_count):
+        yield start_date + timedelta(days=n_days)
