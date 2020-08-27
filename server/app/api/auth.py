9ed8c4d0b27567d678bf82b4ba0c8af4992de038
PetrushynskyiOleksii <petrushynskyi.oleksii@gmail.com>
2020-08-27 16:49:56 +0300

    SWSS-30: Implement refresh access token view

--- server/app/api/auth.py
+++ server/app/api/auth.py
@@ -1,4 +1,4 @@
-"""This module provides user`s auth views."""
+"""This module provides auth views."""
 
 from http import HTTPStatus
 
@@ -6,19 +6,20 @@ from aiohttp import web
 
 from app.utils.errors import SWSDatabaseError
 from app.utils.validators import validate_email, validate_password
-from app.utils.jwt import generate_auth_token
+from app.utils.jwt import generate_token, decode_token
+from app.utils.errors import SWSTokenError
 from app.models.user import User
 
 
 auth_routes = web.RouteTableDef()
 
 
-@auth_routes.view("/user/signup")
-class UserSignUp(web.View):
+@auth_routes.view("/auth/signup")
+class AuthSignUp(web.View):
     """Class that includes functionality to sign up user in system."""
 
     async def post(self):
-        """Create a new user in system and generate auth token for him."""
+        """Create a new user in system."""
         body = self.request.body
         try:
             email, password = body["email"], body["password"]
@@ -47,19 +48,17 @@ class UserSignUp(web.View):
                 status=HTTPStatus.BAD_REQUEST
             )
 
-        token = generate_auth_token(
-            user.id,
-            secret_key=self.request.app.config.SERVER_SECRET,
-            exp_days=self.request.app.config.JWT_EXP_DAYS
-        )
         return web.json_response(
-            data={"success": True, **token},
+            data={
+                "success": True,
+                "message": f"The user was created successfully: {user.email}."
+            },
             status=HTTPStatus.OK
         )
 
 
-@auth_routes.view("/user/signin")
-class UserSignIn(web.View):
+@auth_routes.view("/auth/signin")
+class AuthSignIn(web.View):
     """Class that includes functionality to sign in user in system."""
 
     async def post(self):
@@ -94,12 +93,70 @@ class UserSignIn(web.View):
                 status=HTTPStatus.UNAUTHORIZED
             )
 
-        token = generate_auth_token(
-            user.id,
-            secret_key=self.request.app.config.SERVER_SECRET,
-            exp_days=self.request.app.config.JWT_EXP_DAYS
+        secret = self.request.app.config.SERVER_SECRET
+        token, token_exp = generate_token(
+            secret_key=secret,
+            private_claims={"user_id": user.id},
+            exp_days=self.request.app.config.ACCESS_JWT_EXP_DAYS
+        )
+        refresh_token, refresh_token_exp = generate_token(
+            secret_key=secret,
+            private_claims={"user_id": user.id},
+            exp_days=self.request.app.config.REFRESH_JWT_EXP_DAYS
         )
         return web.json_response(
-            data={"success": True, **token},
+            data={
+                "success": True,
+                "access_token": token, "access_token_exp": token_exp,
+                "refresh_access_token": refresh_token, "refresh_access_token_exp": refresh_token_exp
+            },
+            status=HTTPStatus.OK
+        )
+
+
+@auth_routes.view("/auth/refresh_access")
+class AuthRefreshAccess(web.View):
+    """Class that includes functionality to refresh access token for user."""
+
+    async def post(self):
+        """Return refreshed access token for a user."""
+        body = self.request.body
+        try:
+            refresh_token = body["refresh_access_token"]
+        except KeyError:
+            return web.json_response(
+                data={
+                    "success": False,
+                    "message": "Wrong input. Required field refresh_access_token is not provided."
+                },
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+        secret_key = self.request.app.config.SERVER_SECRET
+        try:
+            payload = decode_token(refresh_token, secret_key)
+        except SWSTokenError as err:
+            return web.json_response(
+                data={"success": False, "message": f"Wrong refresh access token. {str(err)}"},
+                status=HTTPStatus.BAD_REQUEST
+            )
+
+        user_id = payload["user_id"]
+        token, token_exp = generate_token(
+            secret_key=secret_key,
+            private_claims={"user_id": user_id},
+            exp_days=self.request.app.config.ACCESS_JWT_EXP_DAYS
+        )
+        refresh_token, refresh_token_exp = generate_token(
+            secret_key=secret_key,
+            private_claims={"user_id": user_id},
+            exp_days=self.request.app.config.REFRESH_JWT_EXP_DAYS
+        )
+        return web.json_response(
+            data={
+                "success": True,
+                "access_token": token, "access_token_exp": token_exp,
+                "refresh_access_token": refresh_token, "refresh_access_token_exp": refresh_token_exp
+            },
             status=HTTPStatus.OK
         )
