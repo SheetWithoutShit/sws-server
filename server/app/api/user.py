4615503bfbc67b026689f0edc0090047acf7c7e2
PetrushynskyiOleksii <petrushynskyi.oleksii@gmail.com>
2020-08-31 16:46:22 +0300

    SWSS-36: Create view for formation telegram invitation link

--- server/app/api/user.py
+++ server/app/api/user.py
@@ -1,11 +1,12 @@
 """This module provides user`s views."""
 
+import uuid
 from http import HTTPStatus
 
 from aiohttp import web
 
-from app.utils.errors import SWSDatabaseError
 from app.models.user import User
+from app.utils.errors import SWSDatabaseError
 
 
 user_routes = web.RouteTableDef()
@@ -27,5 +28,25 @@ class UserView(web.View):
 
         return web.json_response(
             data={"success": True, "user": user.as_dict()},
+            status=HTTPStatus.OK
+        )
+
+
+@user_routes.view("/user/telegram")
+class UserView(web.View):
+    """Class that includes functionality to work with user telegram chat."""
+
+    async def get(self):
+        """Return invitation link for user to telegram bot."""
+        redis = self.request.app["redis"]
+        config = self.request.app.config
+
+        telegram_cache_code = str(uuid.uuid4())
+        telegram_cache_key = config.TELEGRAM_TEMPLATE.format(code=telegram_cache_code)
+        await redis.set(telegram_cache_key, self.request.user_id, config.TELEGRAM_EXPIRE)
+
+        telegram_invitation_link = config.TELEGRAM_BOT_INVITATION_LINK.format(code=telegram_cache_code)
+        return web.json_response(
+            data={"success": True, "invitation_link": telegram_invitation_link},
             status=HTTPStatus.OK
         )
